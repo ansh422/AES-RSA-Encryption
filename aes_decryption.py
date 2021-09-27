429c08a11b3f3bf0fdf4b523cdb96cb801f977e1
Ansh Gupta <52572592+ansh422@users.noreply.github.com>
2021-09-27 13:14:56 +0530

    Update aes_decryption.py

--- aes_decryption.py
+++ aes_decryption.py
@@ -1,5 +1,6 @@
 
 # File made by Ansh Gupta 2018317
+# Code used from : https://jhafranco.com/2012/02/11/simplified-aes-implementation-in-python/
 
 import socket
 import sys
@@ -16,8 +17,8 @@ sBox_a=[0x9,0x4,0xa,0xb,0xd,0x1,0x8,0x5,
 #Round keys
 w=[None]*6
 
-#This function is used to multiply two polynomials in GF(2^4),i.e, x^4+x+1
 def mult(p1,p2):
+      '''Function to multiply two polynomials in GF(2^4),i.e, x^4+x+1'''
     p=0
     while p2:
         if p2 & 0b1:
@@ -28,24 +29,29 @@ def mult(p1,p2):
         p2 >>=1
     return p & 0b1111
 
-#This function is used to convert the integer into state vector
+
 def IntToVec(n):
+      '''Function to convert the integer into state vector'''
     return [n>>12,(n>>4) & 0xf, (n>>8) & 0xf, n & 0xf]
 
-#Function to convert the state vector back to integer
+
 def VecToInt(n):
+      '''Function to convert the state vector back to integer'''
     return (n[0]<<12)+(n[2]<<8)+(n[1]<<4)+n[3]
  
-#to add the two keys in GF(2^4)
+
 def addKey(s1,s2):
+      '''Function to add the two keys in GF(2^4)'''
     return [i^j for i,j in zip(s1,s2)]
 
-# function used for Nibble Substitution
+
 def NibSub(sbox,s):
+      '''function used for Nibble Substitution'''
     return [sbox[e] for e in s]
 
-#functions to shift the rows of state
+
 def shiftRows(s):
+      '''Function to shift the rows of state'''
     return [s[0],s[1],s[3],s[2]]
 
 #swapping each nibble and substituting it using s-box
@@ -62,13 +68,15 @@ def keyExp(key):
     w[4]=w[2]^Rcon2^sub2Nib(w[3])
     w[5]=w[4]^w[3]
 
-#mixing columns according to the decryption method
+
 def mixCol(s):
+      '''Function to columns according to the decryption method'''
     return [mult(9,s[0])^ mult(2,s[2]), mult(9,s[1])^ mult(2,s[3]),
             mult(9,s[2])^ mult(2,s[0]), mult(9,s[3])^ mult(2,s[1])]
 
-#Function to decrypt the cipherText received from client
+
 def aes_decrypt(data,key):
+      '''Function to decrypt the cipherText received from client'''
     keyExp(key)
     state=IntToVec(((w[4] << 8)+w[5]) ^ data)
     state=NibSub(sBox,shiftRows(state))
