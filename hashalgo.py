af731d482725c9a8c42a896cbe90fd01600afbad
Ansh Gupta <52572592+ansh422@users.noreply.github.com>
2021-09-27 13:18:10 +0530

    Update hashalgo.py

--- hashalgo.py
+++ hashalgo.py
@@ -3,7 +3,8 @@
 
 import hashlib
 
-# returing the digest formed using md5 hash algorithm
+
 def digest(message):
+  '''Function to calculate the digest formed by md5 hash algorithm'''
   m=hashlib.md5(str(message).encode())
   return m.hexdigest()
