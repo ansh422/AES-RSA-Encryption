eb2fccc702cd2a281db95dd8a7de4b0db0536778
Ansh Gupta <52572592+ansh422@users.noreply.github.com>
2021-09-27 13:11:46 +0530

    Update aes_encryption.py

--- aes_encryption.py
+++ aes_encryption.py
@@ -1,18 +1,21 @@
 
 # File made by Ansh Gupta 2018317
+# Code used from : https://jhafranco.com/2012/02/11/simplified-aes-implementation-in-python/
 
 import socket
 import sys
 import json
-#substitution box
+
+#Substitution Box
 sBox=[0x9,0x4,0xa,0xb,0xd,0x1,0x8,0x5,
       0x6,0x2,0x0,0x3,0xc,0xe,0xf,0x7]
 
-#round keys
+#Round Keys
 w=[None]*6
 
-#This function is used to multiply two polynomials in GF(2^4),i.e, x^4+x+1
+
 def mult(p1,p2):
+      '''Function to multiply two polynomials in GF(2^4),i.e, x^4+x+1 '''
     p=0
     while p2:
         if p2 & 0b1:
@@ -23,32 +26,39 @@ def mult(p1,p2):
         p2 >>=1
     return p & 0b1111
 
-#This function is used to convert the integer into state vector
+
 def IntToVec(n):
+      '''Function to convert integer into state vector'''
     return [n>>12,(n>>4) & 0xf, (n>>8) & 0xf, n & 0xf]
 
-#Function to convert the state vector back to integer
+
 def VecToInt(n):
+      '''Function to convert state vector to integer'''
     return (n[0]<<12)+(n[2]<<8)+(n[1]<<4)+n[3]
  
-#to add the two keys in GF(2^4)
+
 def addKey(s1,s2):
+      '''Adding two keys in GF(2^4)'''
     return [i^j for i,j in zip(s1,s2)]
 
-# function used for Nibble Substitution
+
 def NibSub(sbox,s):
+      ''' Function used as Nibble Substitution'''
     return [sbox[e] for e in s]
 
-#functions to shift the rows of state
+
 def shiftRows(s):
+      '''Function to shift rowws of state'''
     return [s[0],s[1],s[3],s[2]]
 
-#swapping each nibble and substituting it using s-box
+
 def sub2Nib(b):
+      '''Swapping each nibble and substituting it using s-box'''
     return sBox[b >>4]+(sBox[b & 0x0f]<<4)
 
-#To generate the keys for encryption and decryption
+
 def keyExp(key):
+      '''To generate the keys for encryption and decryption'''
     Rcon1,Rcon2 = 0b10000000, 0b00110000
     w[0]=(key & 0xff00) >> 8
     w[1]=key & 0x00ff
@@ -57,13 +67,15 @@ def keyExp(key):
     w[4]=w[2]^Rcon2^sub2Nib(w[3])
     w[5]=w[4]^w[3]
 
-#mixing columns according to the encryption method
+
 def mixCol(s):
+      '''Function to mix columns according to the encryption method'''
     return [s[0] ^ mult(4,s[2]), s[1] ^ mult(4,s[3]),
             s[2]^mult(4,s[0]),s[3]^ mult(4,s[1])]
 
-#Function to encrypt the plainText.
+
 def aes_encrypt(data,key):
+    '''Function to encrypt the plainText'''
     keyExp(key)
     state=IntToVec(((w[0]<<8)+w[1])^data)
     state=mixCol(shiftRows(NibSub(sBox,state)))
